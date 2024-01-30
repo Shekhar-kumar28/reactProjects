e59454ec4839af9427c96e3a902f6ae642c1a213
Shekhar Kumar <sk164949@gmail.com>
2024-01-30 19:03:37 +0530

    changes

--- taskmanagement/client/src/components/header/Header.jsx
+++ taskmanagement/client/src/components/header/Header.jsx
@@ -1,10 +1,46 @@
+import './header.scss';
+import '../../styles/components/_button.scss';
+import { useSelector, useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { logoutSuccess } from '../../redux/authSlice';
+import history from '../../history';
 
 const Header = () => {
-  return (
-    <div>
-      <div>Hey i am shekhar</div>
-    </div>
-  )
-}
+	const dispatch = useDispatch();
+	const { auth } = useSelector((state) => ({ ...state }));
 
-export default Header
+	const handleClick = (e) => {
+		e.preventDefault();
+		dispatch(logoutSuccess());
+		localStorage.removeItem('auth');
+		history.push('/signin');
+		window.location.reload();
+	};
+	return (
+		<div>
+			<nav className='header'>
+				<div className='header__logo'>
+					<h5>Task Manager</h5>
+				</div>
+				<div className='header__buttons'>
+					{auth.currentUser && auth.currentUser.token ? (
+						<Link to='/signin' className='button' onClick={handleClick}>
+							SignOut
+						</Link>
+					) : (
+						<>
+							<Link to='/signin' className='button'>
+								SignIn
+							</Link>
+							<Link to='/signup' className='button'>
+								SignUp
+							</Link>
+						</>
+					)}
+				</div>
+			</nav>
+		</div>
+	);
+};
+
+export default Header;
