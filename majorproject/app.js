c45b94a0601f8d2d3b86d06277a54efc398f541e
Shekhar Kumar <sk164949@gmail.com>
2024-01-31 15:01:02 +0530

    2nd part completed

--- majorproject/app.js
+++ majorproject/app.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const Listing = require("./models/listing")
 const path = require("path");
 const methodOverride = require("method-override")
+const ejsMate = require("ejs-mate");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -21,6 +22,8 @@ app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
+app.engine("ejs",ejsMate);
+app.use(express.static(path.join(__dirname,"/public")));
 
 
 app.get("/",(req,res)=>{
--- majorproject/models/listing.js
+++ majorproject/models/listing.js
@@ -2,21 +2,26 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const listingSchema = new Schema({
-    title :{
-        type:String,
-        required:true,
+    title: {
+        type: String,
+        required: true,
     },
-    description:String,
-    image:{
-        type:String,
-        // default:"https://images.unsplash.com/photo-1698115467904-5f05d51a1785?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        set: (v) => v === " " ? "https://images.unsplash.com/photo-1698115467904-5f05d51a1785?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" : v,
+    description: String,
+    image: {
+        type: String,
+        default: "https://images.unsplash.com/photo-1698115467904-5f05d51a1785?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        set: function (v) {
+            // Check if the value is an empty string, null, or undefined
+            if (!v || v.trim() === "") {
+                return "https://images.unsplash.com/photo-1698115467904-5f05d51a1785?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+            }
+            return v;
+        },
     },
-    price:Number,
-    location:String,
-    country:String,
+    price: Number,
+    location: String,
+    country: String,
 });
 
-const Listing = mongoose.model("Listing",listingSchema);
+const Listing = mongoose.model("Listing", listingSchema);
 module.exports = Listing;
-
