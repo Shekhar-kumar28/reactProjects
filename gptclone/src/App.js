9ed3eaee6d8a966072fefe1be69193f775ccdf2f
Shekhar Kumar <sk164949@gmail.com>
2024-02-04 21:34:46 +0530

    changes

---
+++ gptclone/src/App.js
@@ -0,0 +1,36 @@
+import './App.css';
+import gptLogo from './assets/chatgpt.svg';
+import addBtn from './assets/add-30.png';
+import msgIcon from './assets/message.svg';
+import home from './assets/home.svg';
+import saved from './assets/bookmark.svg';
+import rocket from './assets/rocket.svg';
+
+
+function App() {
+  return (
+    <div className="App">
+      <div className="sideBar">
+        <div className="upperSide">
+          <div className="upperSideTop">
+            <img src={gptLogo} alt="Logo" className="logo" /><span className="brand">ChatGPT</span></div>
+          <button className="midBtn"><img src={addBtn} alt="new chat" className="addBtn" />New Chat</button>
+          <div className="upperSideBottom">
+            <button className="query"><img src={msgIcon} alt="Query" />What is Programming ?</button>
+            <button className="query"><img src={msgIcon} alt="Query" />How to use an API ?</button>
+          </div>
+        </div>
+        <div className="lowerSide">
+          <div className="listItems"><img src={home} alt="" className="listItemsImg" />Home</div>
+          <div className="listItems"><img src={saved} alt="" className="listItemsImg" />Saved</div>
+          <div className="listItems"><img src={rocket} alt="" className="listItemsImg" />Upgrade to Pro</div>
+        </div>
+      </div>
+      <div className="main">
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
